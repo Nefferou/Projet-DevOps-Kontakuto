30468621be030b46c0c7423793fdd35c4bec0b24
Ryan-MARIN <95910660+Ryan-MARIN@users.noreply.github.com>
2022-11-14 13:02:51 +0100

    Layout

--- app/src/Components/App.js
+++ app/src/Components/App.js
@@ -1,18 +1,34 @@
-import '../Scss/App.scss';
-import {AppBar, Stack, Typography} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import "../Scss/App.scss";
+import "../Scss/appColors";
+import { AppBar, Stack, TextField, Typography, Box } from "@mui/material";
+// import SearchIcon from "@mui/icons-material/Search";
+import appColors from "./../Scss/appColors";
 
 function App() {
   return (
     <div className="App">
-      <AppBar>
-          <Stack direction={"row"}>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  Kontakuto
-              </Typography>
-
+      <Stack>
+        <AppBar static sx={{ height: "80px", bgcolor: appColors.yellow }}>
+          <Stack
+            direction={"row"}
+            spacing={2}
+            sx={{ flexgrow: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <Typography
+              variant="h2"
+              fontWeight="700"
+              sx={{ textShadow: "3px 3px 5px #0005" }}
+            >
+              kontakuto_
+            </Typography>
+            <TextField variant="filled">AHAHAH</TextField>
           </Stack>
-      </AppBar>
+        </AppBar>
+
+        <Box>
+          <Typography>Kontakuto_</Typography>
+        </Box>
+      </Stack>
     </div>
   );
 }
---
+++ app/src/Scss/appColors.js
@@ -0,0 +1,5 @@
+const appColors = {
+  yellow: "#FF9900",
+};
+
+export default appColors;
